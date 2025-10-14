0ae0a86db550e41ab9993f7271abc9a1ca3b1268
Fenana07 <fe.maierhofer@gmail.com>
2025-10-14 07:42:30 +0200

    demo
    demo

---
+++ demo/src/main/java/at/htlle/demo/SchweizerSchachApplication.java
@@ -0,0 +1,13 @@
+package at.htlle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchweizerSchachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchweizerSchachApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/at/htlle/demo/SchweizerSchachApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchweizerSchachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
